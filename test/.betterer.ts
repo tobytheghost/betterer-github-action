dfdbaa5d063f490e0bc196dccfbe02f970fd3372
Evilweed <mcierpicki@gmail.com>
2021-12-21 00:05:51 +0100

    plx work

--- test/.betterer.ts
+++ test/.betterer.ts
@@ -1,16 +1,14 @@
 import { typescript } from '@betterer/typescript'
-import { tsquery } from '@betterer/tsquery'
-import { regexp } from '@betterer/regexp'
 
 export default {
     'stricter typescript compilation': () =>
-        typescript('test/test-tsconfig.json', {
+        typescript('test-tsconfig.json', {
             strict: true,
             noImplicitAny: true,
             strictNullChecks: true,
             strictFunctionTypes: true,
             strictPropertyInitialization: false,
             useDefineForClassFields: false,
-        })
+        }).include("src/*.ts")
 }
 //.include('test/test-files/*.ts'),
